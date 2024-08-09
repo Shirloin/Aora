473cd010fa717f66b86ab5e31c54532c51a23231
Shirloin <riccardo@binus.ac.id>
2024-08-09 22:33:51 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <View className='flex-1 justify-center items-center bg-slate-800'>
+            <Text className='text-white text-3xl'>App</Text>
+        </View>
+    )
+}
+
+export default App
